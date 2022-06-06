d74a7597b69128e1241d28fdb461108fad8c2510
Yan <bbb.zzzz.bbb@mail.ru>
2022-06-06 22:38:08 +0200

    glgu

--- copy_medium/news_medium/urls.py
+++ copy_medium/news_medium/urls.py
@@ -4,8 +4,10 @@ from .views import *
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article_list'),
-    path('add_comment/', CommentCreateView.as_view(), name='add_comment'),
-    path('<slug:slug>/', ArticleDetailView.as_view(),  name='article'),
+    path('article/<slug:slug>/add_comment/', CommentCreateView.as_view(), name='add_comment'),
+    path('article/<slug:slug>/', ArticleDetailView.as_view(),  name='article'),
+    # path('tags/', post_list, name='post_list'),
+    # path('tags/<slug:slug>', post_list, name='post_list_by_tag'),
 
 
 ]
